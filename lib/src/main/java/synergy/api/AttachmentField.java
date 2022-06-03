97c62fadfa58efe695c56c785c068b617dc3922a
vlobaco <victor@lobaco.es>
2022-06-03 07:42:57 +0200

    Attachment Field

---
+++ lib/src/main/java/synergy/api/AttachmentField.java
@@ -0,0 +1,52 @@
+package synergy.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class AttachmentField extends Field {
+	private URL url;
+	private BufferedReader reader = null;
+	
+	public AttachmentField() {
+		this(null, null, null, null, null);
+	}
+	
+	public AttachmentField(Integer id, String name, String description, Boolean compulsory, URL url) {
+		super(id, name, description, compulsory);
+		this.url = url;
+	}
+
+	public BufferedReader getReader() {
+		if (reader == null) {
+			try {
+				reader = new BufferedReader(
+						new InputStreamReader(getUrl().openStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return reader;
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "AttachmentField [id=" + getId()
+				+ ", name=" + getName()
+				+ ", nescription=" + getDescription() 
+				+ ", compulsory=" + getCompulsory() 
+				+ ", url=" + url
+				+ "]";
+	}
+	
+	
+}
